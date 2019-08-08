0fe86e5c1472127137ed4ef2654bf58d9e13d320
Frank Orozco <Frank_Miguel29@outlook.com>
2019-08-07 18:30:30 -0800

    Director Dbset added

--- Persistence/CinemaDbContext.cs
+++ Persistence/CinemaDbContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Snack> Snack { get; set; }
         public DbSet<Actor> Actor { get; set; }
+        public DbSet<Director> Director { get; set; }
 
 
         public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options) { }
