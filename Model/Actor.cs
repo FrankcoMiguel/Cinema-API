fa132bacf854361332dcf7ab6237a20ffeefd54c
Frank Orozco <Frank_Miguel29@outlook.com>
2019-08-09 03:47:36 -0800

    Actor model updated

--- Model/Actor.cs
+++ Model/Actor.cs
@@ -12,12 +12,12 @@ namespace Model
            // Films = new HashSet<Film>();
 
         }
-        public virtual int ActorId { get; set; }
-        public virtual string FirstName { get; set; }
-        public virtual string LastName { get; set; }
-        public virtual int Age { get; set; }
-        public virtual char Gender { get; set; }
-        public virtual string Rating { get; set; }
+        public int ActorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+        public string Rating { get; set; }
 
         //public ICollection<Starring> Films { get; set; }
 
