369673f220aca37d01454788eeea22e18c299be3
Frank Orozco <Frank_Miguel29@outlook.com>
2019-08-15 22:14:54 -0800

    ActorFilm relationship established

---
+++ Model/ActorFilm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class ActorFilm
+    {
+
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+        public int FilmId { get; set; }
+        public Film Film { get; set; }
+
+    }
+}
