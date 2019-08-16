8946c27328de2b7930498008f17271a6e25cffdb
Frank Orozco <Frank_Miguel29@outlook.com>
2019-08-15 22:30:14 -0800

    Photo model created

---
+++ Model/Photo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Photo
+    {
+
+        public int PhotoId { get; set; }
+        public string Caption { get; set; }
+        public string Url { get; set; }
+
+        public ICollection<Actor> ActorGallery { get; }
+        public ICollection<Director> DirectorGallery { get; }
+        public ICollection<Employee> EmployeeGallery { get; }
+        public ICollection<Film> Billboard { get; }
+        public ICollection<Snack> Menu { get; }
+
+
+    }
+}
