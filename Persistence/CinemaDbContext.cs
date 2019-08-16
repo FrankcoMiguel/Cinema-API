eaab371ac723cc3e7b9392b6b1b11b618c73cb62
Frank Orozco <Frank_Miguel29@outlook.com>
2019-08-16 00:23:31 -0800

    Fluent API added on database context

--- Persistence/CinemaDbContext.cs
+++ Persistence/CinemaDbContext.cs
@@ -20,6 +20,81 @@ namespace Persistence
 
         public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            // Relationship Film - Actor {Many to Many}
+            builder.Entity<ActorFilm>().HasKey(af => new { af.ActorId, af.FilmId });
+
+            builder
+                .Entity<ActorFilm>()
+                .HasOne(af => af.Actor)
+                .WithMany(a => a.Films)
+                .HasForeignKey(af => af.ActorId);
+
+            builder
+                .Entity<ActorFilm>()
+                .HasOne(af => af.Film)
+                .WithMany(a => a.Actors)
+                .HasForeignKey(af => af.FilmId);
+
+
+            // Relationship Director - Film {One to Many}
+            builder
+                .Entity<Director>()
+                .HasMany(d => d.Films)
+                .WithOne(f => f.Director)
+                .HasForeignKey(f => f.DirectorId);
+
+
+
+            // Relationship Staff - Employee {One to Many}
+            builder
+                .Entity<Staff>()
+                .HasMany(s => s.Employees)
+                .WithOne(e => e.Title)
+                .HasForeignKey(e => e.StaffId);
+
+            // Relationship Photo - Actor {One to One}
+            builder
+                .Entity<Photo>()
+                .HasOne(p => p.Actor)
+                .WithOne(a => a.Photo)
+                .HasForeignKey<Actor>(a => a.PhotoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Relationship Photo - Director {One to One}
+            builder
+                .Entity<Photo>()
+                .HasOne(p => p.Director)
+                .WithOne(d => d.Photo)
+                .HasForeignKey<Director>(d => d.PhotoId);
+
+            // Relationship Photo - Film {One to One}
+            builder
+                .Entity<Photo>()
+                .HasOne(p => p.Film)
+                .WithOne(f => f.Photo)
+                .HasForeignKey<Film>(f => f.PhotoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Relationship Photo - Employee {One to One}
+            builder
+                .Entity<Photo>()
+                .HasOne(p => p.Employee)
+                .WithOne(e => e.Photo)
+                .HasForeignKey<Employee>(e => e.PhotoId);
+
+
+            // Relationship Photo - Snack {One to One}
+            builder
+                .Entity<Photo>()
+                .HasOne(p => p.Snack)
+                .WithOne(s => s.Photo)
+                .HasForeignKey<Snack>(s => s.PhotoId);
+
+
+        }
 
 
 
